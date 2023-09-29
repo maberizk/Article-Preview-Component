f61e13cb3929be682fd7edd6dc4497843af72fcb
maberizk <michelle.aberizk@gmail.com>
2023-09-29 12:12:57 -0700

    updated readme

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const button = document.querySelector(".arrow");
 const popup = document.querySelector(".share-popup");
+const popupMobile = document.querySelector(".share-popup-mobile");
+const viewportWidth = window.innerWidth;
 
 button.addEventListener("click", function () {
-  popup.style.visibility = "visible";
+  if (viewportWidth < 500) {
+    popupMobile.style.visibility = "visible";
+  } else if (viewportWidth > 500) {
+    popup.style.visibility = "visible";
+  }
 
   // Add a click event listener to the document to close the popup when clicking outside
   document.addEventListener("click", closePopupOnClickOutside);
@@ -10,8 +16,13 @@ button.addEventListener("click", function () {
 
 // Function to close the popup when clicking outside
 function closePopupOnClickOutside(event) {
-  if (!popup.contains(event.target) && event.target !== button) {
+  if (
+    !popup.contains(event.target) &&
+    event.target !== button &&
+    !popupMobile.contains(event.target)
+  ) {
     popup.style.visibility = "hidden";
+    popupMobile.style.visibility = "hidden";
     document.removeEventListener("click", closePopupOnClickOutside);
   }
 }
